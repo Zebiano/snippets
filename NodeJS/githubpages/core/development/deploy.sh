179d1bf9a7a2faa029b63876a732807a4af4a5b7
Zebiano <sebas1@hotmail.de>
2021-04-21 18:53:37 +0100

    Remove: NodeJS Snippets

--- NodeJS/githubpages/core/development/deploy.sh
+++
@@ -1,9 +0,0 @@
-# Run this through a bash command line
-
-echo "Deploying build to github pages..."
-npm run generate
-rm -rf ../dist && echo "Successfully removed 'dist'."
-rm -rf ../-nuxt && echo "Successfully removed '-nuxt'."
-mv dist ../ && echo "Successfully moved 'dist'".
-mv ../dist/* ../ && echo "Successfully moved 'dist' contents."
-rm -rf ../dist && echo "Successfully removed 'dist'."
